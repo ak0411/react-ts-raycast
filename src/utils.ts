126af7105d44bc7e7319f3ed0d1ef70c628d8226
Anawil Karavek <akaravek@hotmail.com>
2024-07-08 13:58:50 +0700

    removed unused

--- src/utils.ts
+++
@@ -1,9 +0,0 @@
-// Precompute sin and cos values for optimization
-export const sinTable: number[] = new Array(360);
-export const cosTable: number[] = new Array(360);
-
-for (let i = 0; i < 360; i++) {
-  const rad = (i * Math.PI) / 180;
-  sinTable[i] = Math.sin(rad);
-  cosTable[i] = Math.cos(rad);
-}
